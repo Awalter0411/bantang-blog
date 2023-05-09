dc46e731f7d5f1010e75641f2edc201bd9a9583c
bantangzm <bantangzm@gmail.com>
2023-05-10 07:58:02 +0800

    feat: upload

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,16 +2,23 @@ import { NextResponse } from 'next/server';
 import fs from 'fs/promises';
 import readingTime from 'reading-time'
 import { parseMarkdown, readDirRecursive } from '@/utils';
+import os from 'os'
 
 export async function GET(request: Request) {
   const blogPaths: string[] = [];
   const blogData = []
-  await readDirRecursive(blogPaths, '/contents/blog')
+  await readDirRecursive(blogPaths, 'public/contents/blog')
   for (const p of blogPaths) {
     const { content, data } = parseMarkdown(await fs.readFile(p, { encoding: 'utf-8' }))
-    blogData.push({
-      ...data, filename: p.substring(p.lastIndexOf('/') + 1), time: readingTime(content).text
-    })
+    if (os.platform() === 'win32') {
+      blogData.push({
+        ...data, filename: p.substring(p.lastIndexOf('\\') + 1), time: readingTime(content).text
+      })
+    } else {
+      blogData.push({
+        ...data, filename: p.substring(p.lastIndexOf('/') + 1), time: readingTime(content).text
+      })
+    }
   }
   return NextResponse.json({ list: blogData });
 }
